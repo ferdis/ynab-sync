219b342b0345d0f293cc2b9d2be088e32f03f6af
Ferdi Schmidt <ferdi.schmidt@takealot.com>
2020-05-17 16:26:01 +0200

    defer ynab call

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from starlette.middleware import Middleware
 from starlette.staticfiles import StaticFiles
 from starlette.responses import FileResponse, JSONResponse
 from starlette.config import Config
+from starlette.background import BackgroundTask
 from starlette.middleware.cors import CORSMiddleware
 import uvicorn
 import databases
@@ -46,6 +47,23 @@ app = Starlette(
     middleware=[Middleware(CORSMiddleware, allow_origins=['*'])],
 )
 
+def create_ynab_transaction(authorized, data):
+    ynab.TransactionsApi(ynab_client).create_transaction(
+        budget_id=config('YNAB_BUDGET_ID'),
+        data={
+            'transaction': {
+                'account_id': config('YNAB_ACCOUNT_ID'),
+                'date': data['dateTime'],
+                'amount': -1 * int(data['centsAmount']) * 10,
+                'approved': authorized,
+                'memo': data['reference'],
+                'payee_name': data['merchant']['name'],
+                'flag_color': 'blue',
+                'cleared': 'cleared',
+            }
+        }
+    )
+
 
 @app.route('/api/transactions', methods=['GET'])
 async def list_transactions(request):
@@ -64,6 +82,7 @@ async def list_transactions(request):
             } for result in results
         ]
     }
+
     return JSONResponse(content)
 
 
@@ -72,21 +91,7 @@ async def add_transactions(request):
     body = await request.json()
     authorized = True  # random.random() >= .5
 
-
-    ynab.TransactionsApi(ynab_client).create_transaction(
-        budget_id=config('YNAB_BUDGET_ID'),
-        data={
-            'transaction': {
-                'account_id': config('YNAB_ACCOUNT_ID'),
-                'date': body['dateTime'],
-                'amount': -1 * int(body['centsAmount']) * 10,
-                'approved': authorized,
-                'memo': body['reference'],
-                'payee_name': body['merchant']['name'],
-                'flag_color': 'blue'
-            }
-        }
-    )
+    task = BackgroundTask(create_ynab_transaction, authorized=authorized, data=body)
 
     query = transactions.insert().values(
         date=dateutil.parser.isoparse(body['dateTime']),
@@ -96,12 +101,14 @@ async def add_transactions(request):
         merchant=body['merchant']['name'],
         authorized=authorized,
     )
+
     await database.execute(query)
 
     content = {
         'success': authorized
     }
-    return JSONResponse(content)
+
+    return JSONResponse(content, background=task)
 
 
 @app.route('/api/transactions/{id:int}', methods=['GET'])
